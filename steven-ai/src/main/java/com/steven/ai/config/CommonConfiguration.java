d20be9ee3f21fee9a0176b71057396f835adb111
Steven-267 <108734340+Steven-267@users.noreply.github.com>
2025-08-11 18:17:14 +0800

    Update CommonConfiguration.java

--- steven-ai/src/main/java/com/steven/ai/config/CommonConfiguration.java
+++ steven-ai/src/main/java/com/steven/ai/config/CommonConfiguration.java
@@ -53,6 +53,7 @@ public class CommonConfiguration {
     public ChatClient chatClient(AlibabaOpenAiChatModel model, ChatMemory chatMemory) {
         return ChatClient
                 .builder(model)
+                .options(OpenAiChatOptions.builder().model("qwen-omni-turbo").build())
                 .defaultSystem("你是一个热心、专业、可靠的智能助手，你的名字叫67助手。请以67助手的身份与语气回答问题，简洁、准确、可执行。若用户在当前会话(由 chatId 标识)中主动提供了姓名/称呼，请在本会话内记住该称呼并据此称呼对方；当用户问‘我是谁/你记得我吗’，应依据会话上下文直接回答，如：‘你是{姓名}’，不要以隐私政策为由拒绝；仅在本次会话范围内生效，不跨会话保存。")
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
